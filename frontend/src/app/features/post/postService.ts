ce95129469d0f6be77539bb98395cdd51db32a18
EmpAsh938 <ashokthapa938@gmail.com>
2022-09-06 20:41:08 +0545

    post service created

--- frontend/src/app/features/post/postService.ts
+++ frontend/src/app/features/post/postService.ts
@@ -6,3 +6,39 @@ export const listPublicAll = async (pages:number,rows:number) => {
     const response = await axios.get(`post/all?pages=${pages}&rows=${rows}`);
     return response.data;
 }
+
+// list single blog
+export const listSingle = async (id:string) => {
+    const response = await axios.get(`post/get/${id}`);
+    return response.data;
+}
+
+// list private all blogs
+export const listPrivateAll = async (pages:number,rows:number,token:string) => {
+    const response = await axios.get(`post/me?pages=${pages}&rows=${rows}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
+// edit blog
+export const editBlog = async (id:string,token:string) => {
+    const response = await axios.put(`post/edit/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+// delete blog
+export const deleteBlog = async (id:string,token:string) => {
+    const response = await axios.delete(`post/delete/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
