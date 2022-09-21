124ee2ba33f2a3670729b0c86a739f852a9f87f0
EmpAsh938 <ashokthapa938@gmail.com>
2022-09-21 14:51:17 +0545

    markdown array

---
+++ frontend/src/utils/getMarkdown.ts
@@ -0,0 +1,32 @@
+export const tags = [
+    {
+        'header':'##text'
+    },
+    {
+        'italic':'*text*'
+    },
+    {
+        'bold':'**text**'
+    },
+    {
+        'link':'[title][link]'
+    },
+    {
+        'ol': 'num. text'
+    },
+    {
+        'ul': '- text'
+    },
+    {
+        'blockquote': '> text'
+    },
+    {
+        'code': '```text```'
+    },
+    {
+        'image': '![alt][link]'
+    },
+    {
+        'task': '- [mark] text'
+    }
+]
