62afde3452989d264580909b088df4af567c3038
EmpAsh938 <ashokthapa938@gmail.com>
2022-09-22 19:28:46 +0545

    error type idle fixed

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -5,8 +5,9 @@ import { NotificationsType } from "../types/authTypes";
 
 const ErrorMessage = (notifications: NotificationsType) => {
   const {type, message} = notifications;
+
   return (
-    <div className="fixed top-3 right-1 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div className={`${type === 'idle' ? 'hidden': 'fixed top-3 right-1 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'}`}>
       <div className={`${type === 'error' ? 'shrink-0 text-3xl text-red-700' : 'shrink-0 text-3xl text-green-600'}`}>
         {type === 'error' ? 
         (
