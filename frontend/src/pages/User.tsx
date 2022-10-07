f0bec6c837b3e089afcf4dac29f3cd882aeee6f5
EmpAsh938 <ashokthapa938@gmail.com>
2022-10-07 09:26:35 +0545

    new user page

---
+++ frontend/src/pages/User.tsx
@@ -0,0 +1,23 @@
+import Navbar from '../components/Navbar'
+
+const User = () => {
+  return (
+    <>
+        <Navbar />
+        <main>
+            <section>
+                <div>
+                </div>
+                <div>
+                    
+                </div>
+            </section>
+            <section>
+
+            </section>
+        </main>
+    </>
+  )
+}
+
+export default User
