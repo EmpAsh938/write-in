c73f606d462c9ad2714c22f1787654c82168245b
EmpAsh938 <ashokthapa938@gmail.com>
2022-10-13 10:36:20 +0545

    user page profile module styled

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -1,5 +1,6 @@
 import {useParams} from 'react-router-dom'
 import { MouseEvent, useEffect } from 'react'
+import { MdEmail, MdLink, MdLocationOn } from 'react-icons/md';
 
 import Navbar from '../components/Navbar'
 import HomeBlogs from '../components/HomeBlogs'
@@ -52,19 +53,28 @@ const User = () => {
             <section className='flex items-center justify-start gap-2'>
                 <div className='flex flex-col gap-2'>
                     <UserImage profileImage={userProfile.profileImage} fullname={userProfile.fullname} width={300} height={300} />
-                    <div>
-                        <h3>{userProfile.followers.length} followers</h3>
-                        <button onClick={handleFollow}>{(userProfile.followers.includes(user._id)) ? 'Unfollow' : 'follow'}</button>
+                    <div className='p-2'>
+                        <h3 className='font-normal '>{userProfile.followers.length} followers</h3>
+                        <button className='px-2 py-1 text-white bg-green-500 rounded' onClick={handleFollow}>{(userProfile.followers.includes(user._id)) ? 'Unfollow' : 'follow'}</button>
                     </div>
                 </div>
                 <div className='flex flex-col gap-2'>
-                   <div>
-                        <h2>{userProfile.fullname}</h2>
-                      <h3>{userProfile.username}</h3>  
-                       <h3>{userProfile.email}</h3>
-                       <h4>bio</h4>
-                       <a>link</a>
-                       <h4>country</h4>
+                   <div className='flex flex-col'>
+                        <h2 className='mb-0 text-xl capitalize'>{userProfile.fullname}</h2>
+                      <h3 className='mt-0 italic text-gray-600'>@{userProfile.username}</h3>  
+                      <p>{userProfile.bio}</p>
+                       <h3 className='flex items-center gap-2'>
+                          <MdEmail />
+                          <a href={`mailto:${userProfile.email}`}>{userProfile.email}</a> 
+                       </h3>
+                       <h4 className='flex items-center gap-2'>
+                            <MdLink />
+                            <a href={userProfile.website}>{userProfile.website}</a>
+                       </h4>
+                       <h4 className='flex items-center gap-2'>
+                            <MdLocationOn />
+                            <span>{userProfile.country}</span>
+                       </h4>
                    </div> 
                    <div></div> 
                 </div>
