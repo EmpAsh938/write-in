b40f1dea8cd35f9ada074e2c184652fa27e450c1
EmpAsh938 <ashokthapa938@gmail.com>
2022-11-02 20:15:55 +0545

    useless escape removed

--- frontend/src/utils/emailValidator.ts
+++ frontend/src/utils/emailValidator.ts
@@ -1,4 +1,4 @@
-const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const regex = new RegExp(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/);
 
 export const emailValidator = (email:string) => {
     return regex.test(email);
